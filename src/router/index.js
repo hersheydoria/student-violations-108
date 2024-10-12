6520d3f256dec187e76c5ce3d02a4ec54f5a3e8f
hersheydoria <hersyedoria@gmail.com>
2024-10-12 19:00:33 +0800

    fix: correct path to the visitor view

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 // Importing the views
 import LoginView from '@/views/auth/LoginView.vue'
-import VisitorView from '@/views/VisitorView.vue'
+import VisitorView from '@/views/system/VisitorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
